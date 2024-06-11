18ed886c287c65d7149dccb8e2071b9aa912cb1b
GeorgyDzhanaev <georgy.dzhanaev@yandex.ru>
2024-06-11 13:41:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#длина слова
+print(len("staystrong"))
+#сумма и разности
+first=10
+second=20
+summa=first + second
+diff=first - second
+print(summa)
+print(diff)
+#Среднее арифметическое
+first=10,20,30
+second=100,200,300
+third=500,1000,2000
+mean=first + second + third
+mean=sum(mean) / len(mean)
+print (mean)
+#Простые строчки
+first_sting="понедельник"
+second_sting=", вторник"
+print (first_sting+second_sting)
+#сложная формула
+a=3
+b=6
+c=9
+f=(a * b) + (a * c)
+print (f)
+f=45562.5
+print (f ** 3 / 2)
